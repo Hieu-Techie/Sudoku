57a742a2daad11d7067a377d0ea5aaea3cd95ceb
Hieu-Techie <minhhieu04112004@gmail.com>
2024-03-15 20:09:10 +0700

    add sudoku project

--- src/model/Game.java
+++ src/model/Game.java
@@ -5,64 +5,65 @@ public class Game {
 
 	public Game(int[][] array) {
 		board_game = new Node[9][9];
-        initializeNodes(array);
-    }
+		initializeNodes(array);
+	}
 
-    private void initializeNodes(int[][] array) {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-            	board_game[i][j] = new Node(i, j, array[i][j]);
-            }
-        }
-    }
+	private void initializeNodes(int[][] array) {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				board_game[i][j] = new Node(i, j, array[i][j]);
+			}
+		}
+	}
 
-    public Node getNode(int row, int col) {
-        return board_game[row][col];
-    }
-	
+	public Node getNode(int row, int col) {
+		return board_game[row][col];
+	}
+
 	public boolean validate(int row, int col, int value) {
-        return isValidRow(row, value) && isValidColumn(col, value) && isValidBox(row, col, value) && board_game[row][col].getValue() == 0 && value>0 && value<10;
-    }
+		return isValidRow(row, value) && isValidColumn(col, value) && isValidBox(row, col, value)
+				&& board_game[row][col].getValue() == 0 && value > 0 && value < 10;
+	}
 
-    private boolean isValidRow(int row, int value) {
-        for (int i = 0; i < 9; i++) {
-            if (board_game[row][i].getValue() == value) {
-                return false;
-            }
-        }
-        return true;
-    }
+	private boolean isValidRow(int row, int value) {
+		for (int i = 0; i < 9; i++) {
+			if (board_game[row][i].getValue() == value) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-    private boolean isValidColumn(int col, int value) {
-        for (int i = 0; i < 9; i++) {
-            if (board_game[i][col].getValue() == value) {
-                return false;
-            }
-        }
-        return true;
-    }
+	private boolean isValidColumn(int col, int value) {
+		for (int i = 0; i < 9; i++) {
+			if (board_game[i][col].getValue() == value) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-    private boolean isValidBox(int row, int col, int value) {
-        int boxStartRow = row - row % 3;
-        int boxStartCol = col - col % 3;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board_game[boxStartRow + i][boxStartCol + j].getValue() == value) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-    
-    public boolean isSudokuComplete() {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board_game[i][j].getValue() == 0) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+	private boolean isValidBox(int row, int col, int value) {
+		int boxStartRow = row - row % 3;
+		int boxStartCol = col - col % 3;
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (board_game[boxStartRow + i][boxStartCol + j].getValue() == value) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	public boolean isSudokuComplete() {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				if (board_game[i][j].getValue() == 0) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 }
